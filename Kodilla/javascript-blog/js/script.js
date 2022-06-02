4fe7baaa0372006df3e7934e48544bb59fdfff18
Marietta Kasprzak <marietta.kasprzak@gmail.com>
2022-06-02 18:14:11 +0200

    js

--- Kodilla/javascript-blog/js/script.js
+++ Kodilla/javascript-blog/js/script.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log (event);
+
+}
   
-    /* remove class 'active' from all article links  */
-  
+    /* [DONE]remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+for(let activeLink of activeLinks){
+activeLink.classList.remove('active');}
+
     /* add class 'active' to the clicked link */
-  
+activeLink.classList.add('active');
+
     /* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.titles a.active');
+for(let activeArticle of activeArticles){
+activeArticle.classList.remove('active');}
   
     /* get 'href' attribute from the clicked link */
+
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
