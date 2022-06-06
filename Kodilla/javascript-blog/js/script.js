7373346790e1da4b329dd6c1baab3544e1faa3e2
Marietta Kasprzak <marietta.kasprzak@gmail.com>
2022-06-06 20:50:23 +0200

    blog-js

--- Kodilla/javascript-blog/js/script.js
+++ Kodilla/javascript-blog/js/script.js
@@ -38,3 +38,35 @@ const titleClickHandler = function(event){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+
+
+
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    const titleList = querySelector (optTitleListSelector);
+    function clearMessages(){
+    titleList.innerHTML = '';
+}
+
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
