03a22a52eb0b06c77b1f664727f43562613e7b12
Marietta Kasprzak <marietta.kasprzak@gmail.com>
2022-10-01 13:14:21 +0200

    palarnia-kawy

--- Kodilla/projek palarnia-kawy/src/js/script.js
+++ Kodilla/projek palarnia-kawy/src/js/script.js
@@ -1,5 +1,21 @@
-const ulList = document.createElement('ul')
-const liItem = document.createElement('li')
-liItem.textContent = 'czesc'
+import { settings } from './settings.js'
+const app = {
+    initData: function() {
+        const url = settings.db.url + '/' + settings.db.products;
+        this.data = {};
+        fetch(url)
+            .then((rawResponse) => {
+                return rawResponse.json();
+            })
+            .then((parsedResponse) => {
+                this.data.products = parsedResponse;
+            });
+    },
 
-document.appendChild(ulList)
+    init: function() {
+        const thisApp = this;
+        thisApp.initData();
+    },
+}
+
+app.init();
